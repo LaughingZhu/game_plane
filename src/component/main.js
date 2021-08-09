073721118d54d53619cbeb43b3aa0e45769fb4e5
LaughingZhu <474268433@qq.com>
2021-08-09 18:14:59 +0800

    背景类和main页面

---
+++ src/component/main.js
@@ -0,0 +1,23 @@
+/*
+ * @Description:
+ * @Author: LaughingZhu
+ * @Date: 2021-08-09 17:37:08
+ * @LastEditros:
+ * @LastEditTime: 2021-08-09 18:14:01
+ */
+
+import Background from './runtime/backgorund';
+import React, { Component } from 'react';
+
+const ctx = canvas.getContext('2d');
+const databus = new DataBus();
+
+export default class Main extends Component {
+  constructor() {}
+
+  componentDidMount = () => {};
+
+  componentDidUpdate = () => {};
+
+  render() {}
+}
--- src/component/runtime/backgorund.js
+++ src/component/runtime/backgorund.js
@@ -1,7 +1,51 @@
 /*
- * @Description:
+ * @Description: 游戏背景类
  * @Author: LaughingZhu
  * @Date: 2021-08-09 16:38:40
  * @LastEditros:
- * @LastEditTime: 2021-08-09 16:38:41
+ * @LastEditTime: 2021-08-09 17:36:11
  */
+
+import Sprite from '../base/sprites';
+
+const srceenWidth = window.innerWidth;
+const screenHeight = window.innerHeight;
+
+const BG_IMG_SRC = require('../../assets/img/bg.jpg');
+const BG_WIDTH = 512;
+const BG_HEIGHT = 512;
+
+export default class BackGround extends Sprite {
+  constructor(ctx) {
+    super(BG_IMG_SRC, BG_WIDTH, BG_HEIGHT);
+
+    this.top = 0;
+
+    this.render(ctx);
+  }
+
+  render(ctx) {
+    ctx.drawImage(
+      this.img,
+      0,
+      0,
+      this.width,
+      this.height,
+      0,
+      -screenHeight + this.top,
+      srceenWidth,
+      screenHeight,
+    );
+    ctx.drawImage(
+      this.img,
+      0,
+      0,
+      this.width,
+      this.height,
+      0,
+      this.top,
+      screenWidth,
+      screenHeight,
+    );
+  }
+}
